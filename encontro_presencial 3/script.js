21b909cbd4d573fcde66d257089b16a5413cc4d7
FE5-Melqui <melqsantos96@gmail.com>
2021-12-16 21:59:41 -0300

    Encontro presencial 3

---
+++ encontro_presencial 3/script.js
@@ -0,0 +1,35 @@
+function login (){
+var nome = window.prompt("Qual seu nome?")
+var div = document.getElementById("usuario")
+
+    if (typeof nome == 'undefined' || nome == "" ){ 
+    alert('Preencha o campo...')
+    }
+    else{
+        div.innerHTML = `Olá ${nome}, seja bem vindo.`
+    }
+}
+
+function carrinho (idbutton){
+ //debugger
+ if (typeof listaitens == "undefined"){
+    listaitens = []     
+ }
+    var idtrocado = idbutton.replace("btn","item")
+    var iditem = document.getElementById(idtrocado)
+    var modal = document.getElementById("lista")
+    var cont = document.getElementById("contador")
+    var desc = iditem.children[1].textContent
+    var preco = iditem.children[2].textContent
+    modal.innerHTML = ""
+    listaitens.push({
+        item : desc,
+        preço : preco
+    })
+
+    for(var x = 0; x < listaitens.length; x++){
+          
+    modal.innerHTML += `<p> ${listaitens[x].item} - ${listaitens[x].preço} </p>`
+    cont.innerHTML = listaitens.length
+    }
+}
